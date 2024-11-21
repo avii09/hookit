b7dd8b41d10ed7c31304dbedc713c7c75ac54c32
Avantika Kesarwani <avikesar2013@gmail.com>
2024-11-22 02:27:06 +0530

    feat: added csv support

---
+++ pkg/input/csv.go
@@ -0,0 +1,36 @@
+package input
+
+import (
+	"encoding/csv"
+	"os"
+)
+
+func ReadCSV(filePath string) ([]map[string]string, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+	headers, err := reader.Read() // Read the header row
+	if err != nil {
+		return nil, err
+	}
+
+	var rows []map[string]string
+	for {
+		record, err := reader.Read()
+		if err != nil {
+			break
+		}
+
+		row := make(map[string]string)
+		for i, header := range headers {
+			row[header] = record[i]
+		}
+		rows = append(rows, row)
+	}
+
+	return rows, nil
+}
